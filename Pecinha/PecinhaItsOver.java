b9cf5c1ed496f825bb2f22a8faab4eb3395b5055
Bernardo-Lisboa <bernardo.al2006@aluno.ifsc.edu.br>
2025-06-19 20:41:54 -0300

    Update no movimento e na arma
    Movimento "aleatório" e mudança para atirar no ponto futuro do robô inimigo

--- Pecinha/PecinhaItsOver.java
+++ Pecinha/PecinhaItsOver.java
@@ -5,8 +5,9 @@ import robocode.HitRobotEvent;
 import robocode.Robot;
 import robocode.ScannedRobotEvent;
 import static robocode.util.Utils.normalRelativeAngleDegrees;
-
-//import java.awt.Color;
+import java.awt.geom.Point2D;
+import java.util.Random;
+import java.awt.Color;
 
 // API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
 
@@ -15,8 +16,12 @@ import static robocode.util.Utils.normalRelativeAngleDegrees;
  */
 public class PecinhaItsOver extends AdvancedRobot
 {
+	private Random random = new Random();
+	private enum Mode {
+        caso1, caso2, caso3, caso4
+    }
+    private Mode currentMode = Mode.caso1;
 	double moveAmount;
-	boolean peek;
 	int count = 0; // Keeps track of how long we've
 	// been searching for our target
 	double gunTurnAmt; // How much to turn our gun when searching
@@ -31,48 +36,30 @@ public class PecinhaItsOver extends AdvancedRobot
 		// and the next line:
 
 		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
-
+		setBodyColor(Color.BLACK);
+		setGunColor(Color.WHITE);
+		setRadarColor(Color.WHITE);
+		setRadarColor(Color.BLACK);
 		// Robot main loop
-		trackName = null; // Initialize to not tracking anyone
-		setAdjustGunForRobotTurn(true); // Keep the gun still when we turn
-		gunTurnAmt = 10; // Initialize gunTurn to 10
+		trackName = null;
+		setAdjustGunForRobotTurn(true); // mover a arma independentemente
+		setAdjustRadarForGunTurn(true); // mover o radar independentemente
+		gunTurnAmt = 10;
 		moveAmount = Math.max(getBattleFieldWidth(), getBattleFieldHeight());
-		peek = false;
-
-		// turnLeft to face a wall.
-		// getHeading() % 90 means the remainder of
-		// getHeading() divided by 90.
-		turnLeft(getHeading() % 90);
-		ahead(moveAmount);
-		// Turn the gun to turn right 90 degrees.
-		peek = true;
-		turnGunRight(90);
-		turnRight(90);
 		while(true) {
-			// Look before we turn when ahead() completes.
-			peek = true;
-			// Move up the wall
-			ahead(moveAmount);
-			// Don't look now
-			peek = false;
-			// Turn to the next wall
-			turnRight(90);
-			// turn the Gun (looks for enemy)
-			turnGunRight(gunTurnAmt);
-			// Keep track of how long we've been looking
-			count++;
-			// If we've haven't seen our target for 2 turns, look left
-			if (count > 2) {
-				gunTurnAmt = -10;
+			switch (currentMode) {
+                case caso1:
+                    stopAndGoMovement();
+                    break;
+                case caso2:
+                    randomMovement();
+                    break;
+                case caso3:
+                    spiralMovement();
+                    break;
 			}
-			// If we still haven't seen our target for 5 turns, look right
-			if (count > 5) {
-				gunTurnAmt = 10;
-			}
-			// If we *still* haven't seen our target after 10 turns, find another target
-			if (count > 11) {
-				trackName = null;
-			}
+			updateMovementMode();
+			turnRadarRight(360);
 		}
 	}
 
@@ -80,15 +67,21 @@ public class PecinhaItsOver extends AdvancedRobot
 	 * onScannedRobot: What to do when you see another robot
 	 */
 	public void onScannedRobot(ScannedRobotEvent e) {
-		// If the other robot is close by, and we have plenty of life,
-		// fire hard!
-		if (e.getDistance() < 50 && getEnergy() > 50) {
-			fire(3);
-		} // otherwise, fire 1.
-		else {
-			fire(1);
+		double firePower = Math.min(500 / e.getDistance(), 3);
+		double turnGunAmt = normalRelativeAngleDegrees(e.getBearing() + getHeading() - getGunHeading());
+		double bulletSpeed = 20 - firePower * 3;
+		long time = (long)(e.getDistance() / bulletSpeed);
+		double absBearingDeg = (getHeading() + e.getBearing());
+		if (absBearingDeg < 0) absBearingDeg += 360;
+		double x = getX() + Math.sin(Math.toRadians(absBearingDeg)) * e.getDistance();
+		double y = getY() + Math.cos(Math.toRadians(absBearingDeg)) * e.getDistance();
+		double futureX = x + Math.sin(Math.toRadians(getHeading())) * getVelocity() * time;
+		double futureY = y + Math.cos(Math.toRadians(getHeading())) * getVelocity() * time;
+		double absDeg = absoluteBearing(getX(), getY(), futureX, futureY);
+		setTurnGunRight(normalizeBearing(absDeg - getGunHeading()));
+		if (getGunHeat() == 0 && Math.abs(getGunTurnRemaining()) < 10) {
+			setFire(firePower);
 		}
-		// Call scan again, before we turn the gun
 		scan();
 	}
 	/**
@@ -103,21 +96,71 @@ public class PecinhaItsOver extends AdvancedRobot
 	 * onHitWall: What to do when you hit a wall
 	 */
 	public void onHitWall(HitWallEvent e) {
-		// Replace the next line with any behavior you would like
-		back(20);
+		back(50);
+        setTurnRight(90); 
 	}
 	
 	public void onHitRobot(HitRobotEvent e) {
-		double turnGunAmt = normalRelativeAngleDegrees(e.getBearing() + getHeading() - getGunHeading());
-
-		turnGunRight(turnGunAmt);
-		fire(3);
-		// If he's in front of us, set back up a bit.
-		if (e.getBearing() > -90 && e.getBearing() < 90) {
-			back(100);
-		} // else he's in back of us, so set ahead a bit.
-		else {
-			ahead(100);
-		}
+		back(50);
+		setTurnRight(90);
 	}
+	public double normalizeBearing(double angle) {
+	while (angle >  180) angle -= 360;
+	while (angle < -180) angle += 360;
+	return angle;
+	}
+	double absoluteBearing(double x1, double y1, double x2, double y2) {
+	double xo = x2-x1;
+	double yo = y2-y1;
+	double hyp = Point2D.distance(x1, y1, x2, y2);
+	double arcSin = Math.toDegrees(Math.asin(xo / hyp));
+	double bearing = 0;
+	if (xo > 0 && yo > 0) { // both pos: lower-Left
+		bearing = arcSin;
+	} else if (xo < 0 && yo > 0) { // x neg, y pos: lower-right
+		bearing = 360 + arcSin; // arcsin is negative here, actuall 360 - ang
+	} else if (xo > 0 && yo < 0) { // x pos, y neg: upper-left
+		bearing = 180 - arcSin;
+	} else if (xo < 0 && yo < 0) { // both neg: upper-right
+		bearing = 180 - arcSin; // arcsin is negative here, actually 180 + ang
+	}
+	return bearing;
+	}
+
+	private void randomMovement() {
+        double randomDistance = random.nextInt(200) - 100;
+        setAhead(randomDistance);
+        setTurnRight(random.nextInt(360)); // Randomize turn angle
+    }
+	private void spiralMovement() {
+        double turnAngle = 30 + random.nextInt(30);
+        setTurnRight(turnAngle);
+        setAhead(100);
+    }
+	private void strafeMovement() {
+        double strafeDistance = 30 + random.nextInt(60);
+        setMaxVelocity(8);
+        setAhead(strafeDistance);
+        setTurnRight(random.nextInt(90));
+    }
+	private void stopAndGoMovement() {
+        double moveDistance = 50;
+        setMaxVelocity(8);
+        setAhead(moveDistance);
+        execute();
+        setTurnRight(random.nextInt(90));
+    }
+		private void updateMovementMode() {
+        double distanceToEnemy = getDistanceRemaining();
+        if (distanceToEnemy < 100) {
+            currentMode = Mode.caso1;
+        }
+        else if (distanceToEnemy > 200) {
+            currentMode = Mode.caso2;
+        }
+        else {
+            currentMode = Mode.caso3;
+        }
+    }
 }
+
